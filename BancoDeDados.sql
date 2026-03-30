48d949229403a3cc9cba64f4b48367c621edf3bd
Lucas Aguiar <lucas.aguiar1967@gmail.com>
2026-03-30 11:32:36 -0300

    segunda tabela concluida

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -7,4 +7,12 @@ create table livro(
     paginas int,
 ); 
 
+create table alunos(
+    id int primary key AUTO_INCREMENT,
+    nome varchar(25) not null,
+    Gmail varchar (30),
+    turma varchar(30),
+    CGM int primary key AUTO_INCREMENT,
+);
+
 

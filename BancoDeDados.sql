0772baa36ec7e7edc3249658c74ac19baf4b360f
Lucas Aguiar <lucas.aguiar1967@gmail.com>
2026-03-30 11:57:38 -0300

    Trabalho concluido

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -4,7 +4,7 @@ create table livro(
     id int primary key AUTO_INCREMENT,
     titulo varchar(30),
     editora varchar(50),
-    paginas int,
+    paginas int
 ); 
 
 create table alunos(
@@ -12,7 +12,14 @@ create table alunos(
     nome varchar(25) not null,
     Gmail varchar (30),
     turma varchar(30),
-    CGM int primary key AUTO_INCREMENT,
+    CGM int unique
 );
 
-
+create table emprestimo(
+    id int primary key AUTO_INCREMENT,
+    id_alunos int,
+    id_livro int, 
+    data_emprestimo date,
+    data_de_devolucao date,
+    status_do_emprestimo varchar(30)
+);
